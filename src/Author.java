eb38ba7546cf964c15543211b5cd9a7d4c9772cb
Александр <kozin.last@yandex.ru>
2024-05-19 17:02:16 +0300

    Сделал форматирование!

--- src/Author.java
+++ src/Author.java
@@ -14,6 +14,7 @@ public class Author {
     public String getSurnameAuthor() {
         return this.surnameAuthor;
     }
+
     public String toString() {
         return "" + nameAuthor + " " + surnameAuthor;
     }
